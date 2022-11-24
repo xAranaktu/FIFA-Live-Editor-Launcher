14d633efb094d1614a9c30addba73d303250a459
Aranaktu <xAranaktu@pm.me>
2022-11-24 12:19:51 +0100

    fix delay

--- Launcher/main.cpp
+++ Launcher/main.cpp
@@ -103,6 +103,7 @@ int WinMain(
     ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
 
     if (g_Config.auto_inject) {
+        g_Injector.SetDelay(g_Config.injection_delay);
         std::thread t1(&Injector::Inject, &g_Injector);
         t1.detach();
     }
