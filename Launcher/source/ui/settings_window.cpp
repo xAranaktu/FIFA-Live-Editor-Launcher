72510903373312dcc087b92191b62a735f00a6e2
Aranaktu <xAranaktu@pm.me>
2024-06-07 16:05:42 +0200

    ImGuiFileDialog update

--- Launcher/source/ui/settings_window.cpp
+++ Launcher/source/ui/settings_window.cpp
@@ -80,7 +80,10 @@ namespace UIWindows {
             ImGui::PushID("##GameLocFDBTN");
             if (ImGui::Button("...")) {
                 g_GUI.CloseCurrentFileDialog();
-                //ImGuiFileDialog::Instance()->OpenDialog("GameLocFD", "Choose a Directory", nullptr, ".");
+
+                IGFD::FileDialogConfig cfg;
+                cfg.path = ".";
+                ImGuiFileDialog::Instance()->OpenDialog("GameLocFD", "Choose a Directory", nullptr, cfg);
             }
             ImGui::PopID();
             ImGui::SameLine();
@@ -100,7 +103,10 @@ namespace UIWindows {
             ImGui::PushID("##LEDataRootFDBTN");
             if (ImGui::Button("...")) {
                 g_GUI.CloseCurrentFileDialog();
-                //ImGuiFileDialog::Instance()->OpenDialog("LEDataRootFD", "Choose a Directory", nullptr, ".");
+
+                IGFD::FileDialogConfig cfg;
+                cfg.path = ".";
+                ImGuiFileDialog::Instance()->OpenDialog("LEDataRootFD", "Choose a Directory", nullptr, cfg);
             }
             ImGui::PopID();
             ImGui::SameLine();
@@ -111,7 +117,10 @@ namespace UIWindows {
             ImGui::PushID("##ModsRootFDBTN");
             if (ImGui::Button("...")) {
                 g_GUI.CloseCurrentFileDialog();
-                //ImGuiFileDialog::Instance()->OpenDialog("ModsRootFD", "Choose a Directory", nullptr, ".");
+
+                IGFD::FileDialogConfig cfg;
+                cfg.path = ".";
+                ImGuiFileDialog::Instance()->OpenDialog("ModsRootFD", "Choose a Directory", nullptr, cfg);
             }
             ImGui::PopID();
             ImGui::SameLine();
