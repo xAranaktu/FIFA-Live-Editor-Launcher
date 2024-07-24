90378de9ea93bd0776d616efd442d376d6ce5f2c
Aranaktu <xAranaktu@pm.me>
2024-07-24 19:21:36 +0200

    ver up

--- Launcher/headers/consts.h
+++ Launcher/headers/consts.h
@@ -28,17 +28,20 @@ inline const std::map<std::string, std::string> GAME_VERSION_TU_MAP = {
     { "1.0.86.43378",   "TU11"},
     { "1.0.86.54938",   "TU11.1"},
     { "1.0.87.7970",    "TU12"},
-    { "1.0.87.30122",    "TU13"},
-    { "1.0.87.55322",    "TU14"},
-    { "1.0.88.42253",    "TU15"},
-    { "1.0.89.6557",    "TU15.1"}
+    { "1.0.87.30122",   "TU13"},
+    { "1.0.87.55322",   "TU14"},
+    { "1.0.88.42253",   "TU15"},
+    { "1.0.89.6557",    "TU15.1"},
+    { "1.0.89.16451",   "TU16"},
+    { "1.0.89.28334",   "TU17"},
+    { "1.0.90.3389",    "TU18"}
 };
 
 inline const std::vector<std::string> COMPATIBLE_TITLE_UPDATES = {
-    "TU15.1"
+    "TU15.1", "TU16", "TU17", "TU18"
 };
 
-inline const int LATEST_TU = 15;
-inline const int LATEST_MAJOR_GAME_VER = 89;
-inline const int LATEST_MINOR_GAME_VER = 6557;
+inline const int LATEST_TU = 18;
+inline const int LATEST_MAJOR_GAME_VER = 90;
+inline const int LATEST_MINOR_GAME_VER = 3389;
 
