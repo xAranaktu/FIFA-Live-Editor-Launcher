1bb18b5759ff382eab53edf91e9dac248357c5a0
Aranaktu <xAranaktu@pm.me>
2024-12-06 15:51:00 +0100

    ver up

--- Launcher/headers/consts.h
+++ Launcher/headers/consts.h
@@ -4,7 +4,7 @@
 
 inline const int EAFC_EDITION = 25;
 inline const char* TOOL_NAME = "Live Editor";
-inline const char* TOOL_VERSION = "v25.2.4";
+inline const char* TOOL_VERSION = "v25.2.5";
 inline const char* GAME_WINDOW_CLS = "EA SPORTS FC 25";
 inline const char DisclaimerContent[] = {
     "Using this put you in risk of getting banned. I don't know if you will still be able to access your game account after that happen.\nAlso, EA may patch this tool at any time and from that point it may be not possible to update it at all. Enjoy while you can"
@@ -15,7 +15,7 @@ inline const char DisclaimerContent[] = {
 // Game version - Title Update
 // Game version from <GameInstallDir>/__Installer/installerdata.xml
 inline const std::map<std::string, std::string> GAME_VERSION_TU_MAP = {
-    { "1.0.117.47204",     "TU6.1"},
+    { "1.0.117.47204",      "TU6.1"},
     { "1.0.117.36387",      "TU6"},
     { "1.0.117.17964",      "TU5"},
     { "1.0.116.40849",      "TU4"},
@@ -31,5 +31,5 @@ inline const std::vector<std::string> COMPATIBLE_TITLE_UPDATES = {
 
 inline const int LATEST_TU = 6;
 inline const int LATEST_MAJOR_GAME_VER = 117;
-inline const int LATEST_MINOR_GAME_VER = 407204;
+inline const int LATEST_MINOR_GAME_VER = 47204;
 
