a25b0cffacdd7dcf68981bf16aafaaa9a1f21697
Aranaktu <xAranaktu@pm.me>
2025-01-24 13:20:04 +0100

    TU7+ Only

--- Launcher/headers/consts.h
+++ Launcher/headers/consts.h
@@ -15,6 +15,7 @@ inline const char DisclaimerContent[] = {
 // Game version - Title Update
 // Game version from <GameInstallDir>/__Installer/installerdata.xml
 inline const std::map<std::string, std::string> GAME_VERSION_TU_MAP = {
+    { "1.0.118.36628",      "TU8"},
     { "1.0.117.63822",      "TU7"},
     { "1.0.117.47204",      "TU6.1"},
     { "1.0.117.36387",      "TU6"},
@@ -27,10 +28,10 @@ inline const std::map<std::string, std::string> GAME_VERSION_TU_MAP = {
 };
 
 inline const std::vector<std::string> COMPATIBLE_TITLE_UPDATES = {
-    "TU2", "TU3", "TU4", "TU5", "TU6", "TU6.1", "TU7"
+    "TU7", "TU8"
 };
 
-inline const int LATEST_TU = 7;
-inline const int LATEST_MAJOR_GAME_VER = 117;
-inline const int LATEST_MINOR_GAME_VER = 63822;
+inline const int LATEST_TU = 8;
+inline const int LATEST_MAJOR_GAME_VER = 118;
+inline const int LATEST_MINOR_GAME_VER = 36628;
 
