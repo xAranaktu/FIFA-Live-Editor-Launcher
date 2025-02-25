52fa64fb7ff6bf41896f8e7f9bc48c4bfedd83d7
Aranaktu <xAranaktu@pm.me>
2025-02-25 10:17:54 +0100

    new loggers

--- Launcher/source/ui/settings_window.cpp
+++ Launcher/source/ui/settings_window.cpp
@@ -71,6 +71,8 @@ namespace UIWindows {
 
             save_required |= ImGui::Checkbox(localize.Translate("log_try_find_legacy_file").c_str(), &logger_values->log_try_find_legacy_file);
             save_required |= ImGui::Checkbox(localize.Translate("log_load_legacy_file").c_str(), &logger_values->log_load_legacy_file);
+            save_required |= ImGui::Checkbox(localize.Translate("log_career_mode_fcei").c_str(), &logger_values->log_career_mode_fcei);
+            save_required |= ImGui::Checkbox(localize.Translate("log_localize").c_str(), &logger_values->log_localize);
         }
 
         if (ImGuiLoc::CollapsingHeader("debug")) {
