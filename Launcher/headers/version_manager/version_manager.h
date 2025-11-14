f52a8fb9b652a95528e5b1d485632681913689c8
Aranaktu <xAranaktu@pm.me>
2025-11-14 12:08:31 +0100

    ver up

--- Launcher/headers/version_manager/version_manager.h
+++ Launcher/headers/version_manager/version_manager.h
@@ -40,8 +40,8 @@ namespace LE {
         bool is_using_latest_le = true;
 
         std::string latest_version_url = "";
-        std::string latest_tool_version = "v26.1.9";
-        std::string tool_version = "v26.1.9";
+        std::string latest_tool_version = "v26.2.0";
+        std::string tool_version = "v26.2.0";
         std::string game_version = "1.0.0.0";
 
         std::map<std::string, std::string> game_version_map = {
@@ -57,7 +57,7 @@ namespace LE {
         };
 
         std::map<std::string, std::vector<std::string>> game_version_compatibility = {
-            { "1.0.129.4059",       { "v26.1.9", "v26.1.9" }},
+            { "1.0.129.4059",       { "v26.1.9", "v26.2.0" }},
             { "1.0.129.1902",       { "v26.1.6", "v26.1.8" }},
             { "1.0.128.63165",      { "v26.1.6", "v26.1.8" }},
             { "1.0.128.60171",      { "v26.1.6", "v26.1.8" }},
